ea86b7e2b8ab6c4bd77ec53f24109b992b8762fb
sangavi.s <sangavi.s@juspay.in>
2025-03-12 13:38:47 +0530

    commkit

--- src/ex_01_java_Basic/ABC.java
+++ src/ex_01_java_Basic/ABC.java
@@ -1,6 +1,6 @@
 package ex_01_java_Basic;
 
-public class HelloWorld {
+public class ABC {
     public static void main(String[] args) {
         System.out.println("Hello, World! Welcome Sangavi");
     }
---
+++ src/ex_01_java_Basic/Lab001.java
@@ -0,0 +1,7 @@
+package ex_01_java_Basic;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("nothing");
+    }
+}
